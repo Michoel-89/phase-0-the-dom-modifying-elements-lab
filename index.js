0f5699d42264e7a5cc776a76059c03652ab9c6e5
michoel <michoelr111@gmail.com>
2023-05-03 12:27:49 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.querySelector('main');
+main.remove();
+let newHeader = document.createElement('h1');
+let body = document.querySelector('body')
+newHeader.id = 'victory';
+newHeader.textContent = 'Michoel is the champion'
+body.append(newHeader);
